31e7c03b68736a4baedb4787b33c7b45684f4386
antoniogl85 <antoniogl85@gmail.com>
2014-05-10 13:32:24 +0200

    Plot2 code and image

---
+++ plot2.R
@@ -0,0 +1,19 @@
+# Read data
+data <- read.csv2("household_power_consumption.txt", dec = ".", 
+                  colClasses = c(rep("character", times = 2), rep("numeric", times = 7)), 
+                  na.strings = "?")
+# Select indicated rows
+data <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
+# Convert Date and Time variables to Date/Time classes
+data <- cbind("datetime" = strptime(paste(data$Date, data$Time), 
+        format = "%d/%m/%Y %H:%M:%S"), data[,-c(1,2)])
+
+# Open .png file
+png(file = "plot2.png")
+# Set global graphical parameters
+par(mfrow = c(1,1), mar = c(5.1, 4.1, 2.1, 2.1))
+# Plot line graphic
+plot(data$datetime, data$Global_active_power, type = "l", xlab = "", 
+     ylab = "Global Active Power (kilowatts)")
+# Close .png file
+dev.off()
